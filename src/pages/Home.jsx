31e6eec31fab39a11bee9b040d687bf8905da645
JohnnyNWT <jooaaooskt@gmail.com>
2022-09-08 20:31:58 -0300

    requisito 5 - incompleto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import categories from '../__mocks__/categories';
 
 export default class Home extends Component {
+  state = {
+    inputValue: '',
+    saveInput: '',
+    categorias: [],
+  };
+
+  componentDidMount() {
+    this.handleClick();
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleClick = async () => {
+    const api = await getProductsFromCategoryAndQuery();
+    this.setState({ 
+      inputValue: '',
+      saveInput: inputValue,
+      categorias: api,
+    });
+  }
+
   render() {
+    const { inputValue, saveInput, categorias } = this.state;
+
     return (
       <div>
         <label htmlFor="Home">
           <input
+            data-testid="query-input"
             type="text"
-            name=""
+            name="inputValue"
+            value={ inputValue }
             id=""
             placeholder="Digite aqui"
+            onChange={ this.handleChange }
           />
+          <button
+          data-testid="query-button"
+          type="button"
+          name="inputValue"
+          onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
           <Link to="/card">
             <button
               type="button"
               data-testid="shopping-cart-button"
             >
-              Pesquisar
+              Carrinho
             </button>
           </Link>
           <p
@@ -26,6 +67,9 @@ export default class Home extends Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         </label>
+        <section>
+          {console.log(categorias)}
+        </section>
       </div>
     );
   }
