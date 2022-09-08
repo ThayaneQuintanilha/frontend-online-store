fe81b8af5efb21fd6f272454793caa62b6cd96c3
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-09-08 17:56:36 -0300

    Requisito 3 feito, Thay, Leo, Lari, João

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
@@ -11,6 +12,14 @@ export default class Home extends Component {
             id=""
             placeholder="Digite aqui"
           />
+          <Link to="/card">
+            <button
+              type="button"
+              data-testid="shopping-cart-button"
+            >
+              Pesquisar
+            </button>
+          </Link>
           <p
             data-testid="home-initial-message"
           >
